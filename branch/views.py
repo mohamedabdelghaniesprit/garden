b0e6349ba6bc940c94cc78e2a2e13269ab8877ed
vladymyrgo <vladymyrgo@hotmail.com>
2014-10-19 18:02:43 +0300

    root url to admin

--- branch/views.py
+++ branch/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
 
-# Create your views here.
+
+def home(request):
+    return HttpResponseRedirect('admin/')
--- garden/urls.py
+++ garden/urls.py
@@ -4,9 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'garden.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'branch.views.home', name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
